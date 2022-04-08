dd5885f3291aae8c581cbb94346d5634620e583c
Yannis <yannis182@outlook.fr>
2022-04-08 18:28:20 +0200

    Coming soon page for Up Heal DAO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Stack, Container, Heading, Text, Image, Button, Link } from '@chakra-ui/react'
+
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-    </div>
+    <Stack spacing={"34px"} alignItems={'center'} position={'fixed'} top="50%" left="50%" transform={'translate(-50%, -50%)'}>
+      <Image boxSize='150px' src='/images/website-logo.png' alt='Up Heal Logo' />
+      <Container w={{ base: '400px', md: 'full' }}>
+        <Heading fontFamily={'rubik'} textAlign={'center'} fontSize={'36px'}>
+          Up Heal DAO is Coming Soon.
+        </Heading>
+      </Container>
+      <Container w={{ base: '400px', md: 'full' }}>
+        <Text fontSize={'18px'} fontWeight={600} color="#b4b4b4" textAlign={'center'}>
+          We are a social DAO leveraging Web3 to build DeFi tools and help funding Mental Health initiatives and organisations.
+        </Text>
+      </Container>
+      <Link href='https://discord.gg/S6BZJTgt' isExternal>
+        <Button background={'transparent'} border={'1px solid #ffb7c2'}>
+          Join the community
+        </Button>
+      </Link>
+    </Stack>
   )
 }
 
 export default Home
+

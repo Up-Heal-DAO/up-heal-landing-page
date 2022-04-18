0ec264885f1e2c576c3d7fc96a5d89c99035e26d
Yannis <yannis182@outlook.fr>
2022-04-18 22:16:22 +0200

    added new font and progress on the design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
+import { Fonts } from "../Fonts/fonts"
 
 
 const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat",
+    body: "Montserrat",
+  },
   styles: {
     global: {
       html: {
@@ -21,6 +26,7 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next'
-import { Button, Stack, Flex, Container, Image, Text, Heading } from '@chakra-ui/react'
+import { Button, Stack, Flex, Container, Image, Text, Heading, Box } from '@chakra-ui/react'
 
 
 const Home: NextPage = () => {
   return (
-    <Stack>
+    <Stack display={'flex'} justifyContent="center" alignItems={'center'}>
       <Flex justify={'space-between'} alignItems={'center'} width={'full'} height={{ base: '56px', md: '120px' }} background='white'>
         <Container display={'flex'} alignItems="center" justifyContent={'flex-start'}>
           <Image src='/images/logo-navbar.png' w={'56px'} h={'66px'} alt='Dan Abramov' />
@@ -16,9 +16,13 @@ const Home: NextPage = () => {
           </Button>
         </Container>
       </Flex>
-      <Heading fontSize={'54px'} textAlign={'center'} color={'white'}>
-        Up Heal DAO
-      </Heading>
+      <Box width={'35%'}>
+        <Image position={'absolute'} left="-4.72%" right={'75.76%'} top="17.46%" bottom={'75.3%'} src='/images/line-1.png' w={'417px'} h={'402.84px'} alt='Dan Abramov' />
+        <Heading mt={'300px'} fontSize={'54px'} textAlign={'center'} color={'white'}>
+          Up Heal DAO
+        </Heading>
+        <Text fontSize={'32px'} color={'white'} textAlign="center">It never fell so good be a founder. For mental health, for people.</Text>
+      </Box>
     </Stack >
   )
 }

0f128896e5d9dfa9611d2ae3c7dd385ad3a05bda
Yannis <yannis182@outlook.fr>
2022-04-18 20:15:47 +0200

    progress on styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        height: '100%'
+      },
+      body: {
+        bgGradient: 'linear-gradient(0deg, #FF8295 51.22%, rgba(255, 182, 193, 0.5) 114.97%)',
+      },
+    },
+  },
+})
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,25 @@
 import type { NextPage } from 'next'
-import { Stack, Container, Heading, Text, Image, Button, Link } from '@chakra-ui/react'
+import { Button, Stack, Flex, Container, Image, Text, Heading } from '@chakra-ui/react'
 
 
 const Home: NextPage = () => {
   return (
-    <Stack spacing={"34px"} alignItems={'center'} position={'fixed'} top="50%" left="50%" transform={'translate(-50%, -50%)'}>
-      <Image boxSize='150px' src='/images/website-logo.png' alt='Up Heal Logo' />
-      <Container w={{ base: '400px', md: 'full' }}>
-        <Heading fontFamily={'rubik'} textAlign={'center'} fontSize={'36px'}>
-          Up Heal DAO is Coming Soon.
-        </Heading>
-      </Container>
-      <Container w={{ base: '400px', md: 'full' }}>
-        <Text fontSize={'18px'} fontWeight={600} color="#b4b4b4" textAlign={'center'}>
-          We are a social DAO leveraging Web3 to build DeFi tools and help fund Mental Health initiatives and organizations.
-        </Text>
-      </Container>
-      <Link href='https://discord.gg/S6BZJTgt' isExternal>
-        <Button background={'transparent'} border={'1px solid #ffb7c2'}>
-          Join the community
-        </Button>
-      </Link>
-    </Stack>
+    <Stack>
+      <Flex justify={'space-between'} alignItems={'center'} width={'full'} height={{ base: '56px', md: '120px' }} background='white'>
+        <Container display={'flex'} alignItems="center" justifyContent={'flex-start'}>
+          <Image src='/images/logo-navbar.png' w={'56px'} h={'66px'} alt='Dan Abramov' />
+        </Container>
+        <Container display={'flex'} justifyContent={'flex-end'} alignItems="center">
+          <Text color={'#FF8295'} mr={3}>Staking</Text>
+          <Button colorScheme='white' color={'white'} borderRadius={'24px'} bg={'#FF8295'}>
+            Connect Wallet
+          </Button>
+        </Container>
+      </Flex>
+      <Heading fontSize={'54px'} textAlign={'center'} color={'white'}>
+        Up Heal DAO
+      </Heading>
+    </Stack >
   )
 }
 

81cc1b955748fb96db40d2bed15909de4ce83186
Yannis <yannis182@outlook.fr>
2022-04-20 11:42:18 +0200

    removed navbar space

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,6 @@ import { Button, Flex, Container, Image, Text, Heading, Box } from '@chakra-ui/r
 
 export const Navbar = () => {
   return (
-
     <Flex justify={'space-between'} alignItems={'center'} width={'full'} height={{ base: '56px', md: '120px' }} background='white'>
       <Container display={'flex'} alignItems="center" justifyContent={'flex-start'}>
         <Image src='/images/logo-navbar.png' w={'56px'} h={'66px'} alt='Dan Abramov' />

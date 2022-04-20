db363578d29459e417a7c94c0682ed37171ef041
Yannis <yannis182@outlook.fr>
2022-04-20 11:41:35 +0200

    moved heading section to index component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,19 @@ const Home: NextPage = () => {
   return (
     <Stack bgGradient={'linear-gradient(0deg, #FF8295 51.22%, rgba(255, 182, 193, 0.5) 114.97%)'} display={'flex'} justifyContent="center" alignItems={'center'}>
       <Navbar />
+      <Box width={'735px'} height={'534px'}>
+        <Image position={'absolute'} left="-4.72%" right={'75.76%'} top="17.46%" bottom={'75.3%'} src='/images/line-1.png' w={'417px'} h={'402.84px'} alt='Line design' />
+        <Image position={'absolute'} left="74.54%" right={'-5.54%'} top="15.32px" bottom={'65.83%'} src='/images/line-2.png' w={'446.48px'} h={'500.79px'} alt='Line design' />
+        <Heading mt={'150px'} fontSize={'54px'} textAlign={'center'} color={'white'}>
+          Up Heal DAO
+        </Heading>
+        <Text fontSize={'32px'} color={'white'} textAlign="center">We are funding Mental Health Organizations and community initiatives.</Text>
+        <Flex justifyContent={'center'}>
+          <Button mt={'150px'} color={'#FF8295'} borderRadius={'24px'} bg={'white'}>
+            Join the community
+          </Button>
+        </Flex>
+      </Box>
       <Container maxWidth={'container.xl'} p={0}>
         <Flex py={20}>
           <VStack w="full" h="full" p={10} spacing={10} alignItems={'flex-start'} flexDirection={'row'}>

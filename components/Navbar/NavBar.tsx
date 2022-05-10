ad63c84e1fa0eedad386d9a233bac0eba9eb8505
Sd <shubham.doharey@gmail.com>
2022-05-11 02:42:31 +0530

    Update NavBar.tsx
    Created a responsive Navigator bar different for desktop and mobile.
    You can add more Items in the navbar under the <menuitem> tag.

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -1,28 +1,110 @@
-import { Button, Flex, Container, Image, Link, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+import React from "react";
+import { Link, Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
 
+import Logo from "./Logo";
 
-export const Navbar = () => {
+export const NavBar = (props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <Flex justify={'space-between'} alignItems={'center'} width={'full'} h={'100px'} background='white'>
-      <Container display={'flex'} alignItems="center" justifyContent={'flex-start'}>
-        <Image src='/images/up-heal-logo-website.png' w={'80px'} h={'80px'} alt='Up Heal Logo' />
-      </Container>
-      <Container display={'flex'} justifyContent={'space-evenly'} alignItems="center">
-        <Link href='https://forum.uphealdao.xyz/' isExternal>
-          <Text fontSize={'16px'} fontWeight={500} lineHeight={'19.5px'} color={'brand.100'}>Community</Text>
-        </Link>
-        <Link href='https://snapshot.org/#/upheal.eth' isExternal>
-          <Text fontSize={'16px'} fontWeight={500} lineHeight={'19.5px'} ml={'3px'} mr={'3px'} color={'brand.100'}>Governance</Text>
-        </Link>
-        <Link href='https://discord.gg/QkjdaQzrx3' isExternal>
-          <Button size='lg' position={'relative'} zIndex={10} colorScheme='white' color={'white'} borderRadius={'24px'} bg={'brand.100'}>
+    <NavBarContainer {...props}>
+      <Logo
+        w="80px"
+        ml={5}
+        color={["white", "white", "primary.500", "primary.500"]}
+      />
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <MenuLinks isOpen={isOpen} />
+    </NavBarContainer>
+  );
+};
+
+const CloseIcon = () => (
+  <svg width="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+    <title>Close</title>
+    <path
+      fill="white"
+      d="M9.00023 7.58599L13.9502 2.63599L15.3642 4.04999L10.4142 8.99999L15.3642 13.95L13.9502 15.364L9.00023 10.414L4.05023 15.364L2.63623 13.95L7.58623 8.99999L2.63623 4.04999L4.05023 2.63599L9.00023 7.58599Z"
+    />
+  </svg>
+);
+
+const MenuIcon = () => (
+  <svg
+    width="24px"
+
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="white"
+  >
+    <title>Menu</title>
+    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+  </svg>
+);
+
+const MenuToggle = ({ toggle, isOpen }) => {
+  return (
+    <Box mr={5} display={{ base: "block", md: "none" }} onClick={toggle}>
+      {isOpen ? <CloseIcon /> : <MenuIcon />}
+    </Box>
+  );
+};
+
+const MenuItem = ({ children, isLast, to = "/", ...rest }) => {
+  return (
+    <Link href={to}>
+      <Text display="block" {...rest}>
+        {children}
+      </Text>
+    </Link>
+  );
+};
+
+const MenuLinks = ({ isOpen }) => {
+  return (
+    <Box
+      mr={5} display={{ base: isOpen ? "block" : "none", md: "block" }}
+      flexBasis={{ base: "100%", md: "auto" }}
+    >
+      <Stack
+        spacing={50}
+        align="center"
+        justify={["space-between"]}
+        direction={["column", "row", "row", "row"]}
+        pt={[4, 4, 0, 0]}
+      >
+        <MenuItem to="https://forum.uphealdao.xyz/" fontSize={'16px'} fontWeight={500} lineHeight={'19.5px'} color={'brand.100'}>Community</MenuItem>
+        <MenuItem to="https://snapshot.org/#/upheal.eth" fontSize={'16px'} fontWeight={500} lineHeight={'19.5px'} color={'brand.100' } >Governance </MenuItem>
+        <MenuItem to="https://discord.gg/QkjdaQzrx3" isLast>
+            <Button size='lg' position={'relative'} zIndex={10} colorScheme='white' color={'white'} borderRadius={'24px'} bg={'brand.100'}>
             Discord
           </Button>
-        </Link>
-      </Container>
+        </MenuItem>
+      </Stack>
+    </Box>
+  );
+};
+
+const NavBarContainer = ({ children, ...props }) => {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={3}
+      p={3}
+      bg={["primary.500", "primary.500", "white", "white"]}
+      color={["white", "white", "primary.700", "primary.700"]}
+      {...props}
+    >
+      {children}
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default NavBar;

28d85abb77232f69d1dc703bbc737bf98587e7e1
Yannis <yannis182@outlook.fr>
2022-05-11 20:20:24 +0200

    fixed props in the logo

--- components/Navbar/logo.tsx
+++ components/Navbar/logo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Box, Text, Image} from "@chakra-ui/react";
+import { Box, Image, ChakraProps, OmitCommonProps } from "@chakra-ui/react";
 
-export default function Logo(props) {
+export default function Logo(props: JSX.IntrinsicAttributes & OmitCommonProps<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, keyof ChakraProps> & ChakraProps & { as?: "div" | undefined; }) {
   return (
     <Box {...props}>
-    <Image src='/images/up-heal-logo-website.png' w={'80px'} h={'80px'} alt='Up Heal Logo' />
+      <Image src='/images/up-heal-logo-website.png' w={'80px'} h={'80px'} alt='Up Heal Logo' />
     </Box>
   );
 }
